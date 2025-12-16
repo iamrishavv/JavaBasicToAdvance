637857089760bf252c0909057dce614b5a8f0efd
Rishav <95118742+iamrishavv@users.noreply.github.com>
2025-12-16 23:27:11 +0530

    Add files via upload

---
+++ terneryOperrator.java
@@ -0,0 +1,9 @@
+package BasicJava;
+
+public class terneryOperrator {
+    public static void main(String[] args) {
+        int a=2;
+        int b=(a==1)? 20:30;
+        System.out.println(b);
+    }
+}
