34486052b3170f3c0ffa7f9a349219e95580e254
Rishav <95118742+iamrishavv@users.noreply.github.com>
2025-12-16 10:31:12 +0530

    Add files via upload

---
+++ minAndMaxInteger.java
@@ -0,0 +1,11 @@
+package BasicJava;
+
+public class minAndMaxInteger {
+    public static void main(String[] args) {
+        int x=Integer.MIN_VALUE;
+        int y=Integer.MAX_VALUE;
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
