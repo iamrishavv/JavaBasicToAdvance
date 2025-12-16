67afeb468e64418e1eb68674dc6e1090127d40fd
Rishav <95118742+iamrishavv@users.noreply.github.com>
2025-12-16 11:01:10 +0530

    Add files via upload

---
+++ primitveDataTypeChallenge.java
@@ -0,0 +1,11 @@
+package BasicJava;
+
+public class primitveDataTypeChallenge {
+    public static void main(String[] args) {
+        byte x=10;
+        short y=20;
+        int z=30;
+        long a =50000L+10*(x+y+z);
+        System.out.println(a);
+    }
+}
