3f46808a16c6239b9cbc8ece452b2b36b71eb44f
Rishav <95118742+iamrishavv@users.noreply.github.com>
2025-12-16 23:06:15 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,17 @@
+package BasicJava;
+
+public class operator {
+    public static void main(String[] args) {
+        int a=10;
+        int b=12;
+        int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        int f=a%b;
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(e);
+        System.out.println(f);
+        
+    }
+}
