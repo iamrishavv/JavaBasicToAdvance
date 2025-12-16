9222e4eb237c63750b8d117ece76af79ee5617b1
Rishav <95118742+iamrishavv@users.noreply.github.com>
2025-12-16 23:10:27 +0530

    Add files via upload

---
+++ incrementAndDecrement.java
@@ -0,0 +1,11 @@
+package BasicJava;
+
+public class incrementAndDecrement {
+    public static void main(String[] args) {
+        int a=12;
+        System.out.println(a++);
+        System.out.println(++a);
+       System.out.println(a--);
+       System.out.println(--a);
+    }
+}
