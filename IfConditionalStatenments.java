59fc379f6ecfec61739a2ced2fcffcbdbac48b91
Rishav <95118742+iamrishavv@users.noreply.github.com>
2025-12-17 00:12:30 +0530

    Add files via upload

---
+++ IfConditionalStatenments.java
@@ -0,0 +1,16 @@
+package BasicJava;
+
+public class IfConditionalStatenments {
+    public static void main(String[] args) {
+        int marks =80;
+        if(marks >=90){
+            System.out.println("You got A grade ");
+        }else if(marks >=70){
+            System.out.println("You got B grade");
+        }else if(marks>=50){
+            System.out.println("You passed");
+        }else{
+            System.out.println("You failed");
+        }
+    }
+}
