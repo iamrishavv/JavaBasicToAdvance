e6569edefd4370d36a3bf9c62b7850c94b6ae048
Rishav <95118742+iamrishavv@users.noreply.github.com>
2025-12-26 16:17:17 +0530

    Add files via upload

---
+++ localVariable.java
@@ -0,0 +1,9 @@
+package BasicJava;
+//defined inside a method/constructor/block.
+//it is not accessed outside.
+public class localVariable {
+    public static void main(String[] args) {
+        int a=13;
+        System.out.println(a);
+    }
+}
